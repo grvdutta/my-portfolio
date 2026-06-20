2fafd5ee92f22e512e6bbb92ed84161ccc5fbf7b
grvdutta <grvduttaiem@gmail.com>
2026-06-20 17:29:31 +0530

    Created Footer Section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="border-t border-cyan-500/10 py-6 text-center text-gray-500">
+      <p>
+        © {new Date().getFullYear()} GD Portfolio. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
